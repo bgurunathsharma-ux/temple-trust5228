79e13c78ab3b0b86aaaa9109822771287be644d6
bgurunathsharma-ux <b.gurunathsharma@gmail.com>
2026-01-05 16:34:48 +0530

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -1,10 +1,15 @@
 
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import App from './App.tsx';
+import App from './App';
 
-const rootElement = document.getElementById('root');
-if (rootElement) {
-  const root = createRoot(rootElement);
-  root.render(<App />);
+const container = document.getElementById('root');
+
+if (container) {
+  const root = createRoot(container);
+  root.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
 }
